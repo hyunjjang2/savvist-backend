8812ed235652108e45fc5daba16d975a87c0567f
LSH <0867sh@naver.com>
2024-03-07 01:49:44 +0900

    이메일 로그인 방식에서 아이디 로그인 방식으로 변경

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -6,12 +6,12 @@ const User = require('../models/user');
 
 module.exports = () => {
   passport.use(new LocalStrategy({
-    usernameField: 'email',
+    usernameField: 'userid',
     passwordField: 'password',
     passReqToCallback: false,
-  }, async (email, password, done) => {
+  }, async (userid, password, done) => {
     try {
-      const exUser = await User.findOne({ where: { email } });
+      const exUser = await User.findOne({ where: { userid } });
       if (exUser) {
         const result = await bcrypt.compare(password, exUser.password);
         if (result) {
