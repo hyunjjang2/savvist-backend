c27508d9fb1353dbe487bcc438ae0bf210191d67
LSH <0867sh@naver.com>
2024-03-07 00:57:23 +0900

    유저 모델 수정

--- models/user.js
+++ models/user.js
@@ -3,31 +3,36 @@ const Sequelize = require('sequelize');
 module.exports = class User extends Sequelize.Model {
     static init(sequelize) {
         return super.init({
-            name: {
-                type: Sequelize.STRING(20),
+            username: {
+                type: Sequelize.STRING(15),
                 allowNull: false,
             },
-            nick: {
-                type: Sequelize.STRING(20),
+            userid: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+                unique: true,
+            },
+            nickname: {
+                type: Sequelize.STRING(15),
                 allowNull: false,
                 unique: true,
             },
             password: {
-                type: Sequelize.STRING(100),
+                type: Sequelize.STRING(150),
                 allowNull: false,
             },
             phone: {
-                type: Sequelize.STRING(20),
+                type: Sequelize.STRING(15),
                 allowNull: false,
                 unique: true,
             },
             email: {
-                type: Sequelize.STRING(200),
+                type: Sequelize.STRING(50),
                 allowNull: false,
                 unique: true,
             },
             adress: {
-                type: Sequelize.STRING(200),
+                type: Sequelize.STRING(300),
                 allowNull: false,
             },
             created_at: {
