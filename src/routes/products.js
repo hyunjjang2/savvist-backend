cd2bce91422b0d9defa0d0fed1bda45ddbd1cb0d
LSH <0867sh@naver.com>
2024-03-11 20:23:57 +0900

    상품 이미지 업로드 로직 구현

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,60 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
 const Product = require('../../models/product');
+const { Seller } = require('../../models');
 
 const router = express.Router();
 
-router.post('/register', async(req, res, next)=>{
+try {
+    fs.readdirSync('products');
+}catch (err){
+    console.log('products 폴더가 없어 products 폴더를 생성합니다.');
+    fs.mkdirSync('products');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req,file,cb) {
+            cb(null,'products/');
+        },
+        filename(req,file,cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname,ext) + Date.now() + ext);
+        },
+    }),
+    limits: {fileSize: 5*1024*1024},
+});
+
+
+router.get('/', async(req, res, next)=>{
+    try{
+        const AllProducts = await Product.findAll({
+            include: [{
+                model: Seller,
+                attributes: ['nickname']
+            }],
+        });
+        res.status(200).send(AllProducts);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+
+});
+
+router.post('/register',upload.single('img_url'), async(req, res, next)=>{
+    
     try{
         const {title,price,description,img_url,id} = req.body;
+        const imageUrl = req.file.path;
         await Product.create({
             title,
             price,
             description,
-            img_url,
+            img_url:imageUrl,
             seller_id : id,
         });
         res.status(200).send("상품이 등록되었습니다.");
